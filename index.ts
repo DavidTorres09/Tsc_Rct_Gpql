04577e741bd829e52d98d6273c91bb0012258031
DavidTorres09 <angeldtm09@gmail.com>
2023-04-17 18:43:17 -0400

    HTML

--- index.ts
+++ index.ts
@@ -30,16 +30,26 @@ const resolvers = {
     user: (_parent: any, { id }: { id: number }) => prisma.user.findUnique({ where: { id } }),
   },
   Mutation: {
-    createUser: (_parent: any, { name, email }: { name: string; email: string }) =>
-      prisma.user.create({ data: { name, email } }),
-    updateUser: (_parent: any, { id, name, email }: { id: number; name?: string; email?: string }) =>
-      prisma.user.update({ where: { id }, data: { name, email } }),
-      deleteUser: (_parent: any, { id }: { id: number }) => prisma.user.delete({ where: { id } }),
+    createUser: async (_parent: any, { name, email }: { name: string; email: string }) => {
+      const user = await prisma.user.create({ data: { name, email } });
+      return { success: true, message: `User with ID ${user.id} created successfully`, user };
     },
-  };
+    updateUser: async (_parent: any, { id, name, email }: { id: number; name?: string; email?: string }) => {
+      const updatedUser = await prisma.user.update({ where: { id }, data: { name, email } });
+      return { success: true, message: `User with ID ${updatedUser.id} updated successfully`, user: updatedUser };
+    },
+    deleteUser: async (_parent: any, { id }: { id: number }) => {
+      const deletedUser = await prisma.user.delete({ where: { id } });
+      return { success: true, message: `User with ID ${deletedUser.id} deleted successfully`, user: deletedUser };
+    },
+  },
+};
+
   
   const server = new ApolloServer({ typeDefs, resolvers });
   
   server.listen().then(({ url }) => {
     console.log(`🚀 Server ready at ${url}`);
   });
+
+  
